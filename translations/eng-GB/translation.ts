69dee8b534a989575f939b20a148ef99b6e015e0
Simon Boyer <simon.boyer@openwide.fr>
2012-12-12 11:31:00 +0100

    Add french translation

---
+++ translations/eng-GB/translation.ts
@@ -0,0 +1,111 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS>
+  <context>
+    <name>owtranslate</name>
+    <message>
+      <source>Translation</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Generation</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Generation files translation</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>The generation passed well</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Go to translation list</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>A problem arose during the files generation</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Validate generation</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Send generation</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Cancel generation</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Do you really want to cancel the generation?</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Choose the extensions to be crossed for the generation</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Choose context</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Choose language</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Search</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Reset</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>This extension aims at generating a file of translation by language, and at being able to modify them in the interface of the backOffice of eZ.</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Before anything else, you have to generate the files of translations to be homogeneous.</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Translator / List</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>translations</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Context name Translation</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Click to open</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Validate translation</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Send traduction</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Cancel translation</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Do you really want to cancel the translation?</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>Edit translation</source>
+      <translation></translation>
+    </message>
+  <message><source>List</source><translation></translation></message><message><source>Search translation key</source><translation></translation></message><message><source>Translation key</source><translation></translation></message><message><source>Translator / Search</source><translation></translation></message></context>
+</TS>
---
+++ translations/fre-FR/translation.ts
@@ -0,0 +1,123 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS>
+  <context>
+    <name>owtranslate</name>
+    <message>
+      <source>Translation</source>
+      <translation>Traduction</translation>
+    </message>
+    <message>
+      <source>Generation</source>
+      <translation>Génération</translation>
+    </message>
+    <message>
+      <source>Liste</source>
+      <translation>Liste</translation>
+    </message>
+    <message>
+      <source>Generation files translation</source>
+      <translation>Générer les fichiers de traduction</translation>
+    </message>
+    <message>
+      <source>The generation passed well</source>
+      <translation>La génération s'est effectuée correctement</translation>
+    </message>
+    <message>
+      <source>Go to translation list</source>
+      <translation>Aller à la liste des traductions</translation>
+    </message>
+    <message>
+      <source>A problem arose during the files generation</source>
+      <translation>Un problème a eu lieu lors de la génération des fichiers.</translation>
+    </message>
+    <message>
+      <source>Validate generation</source>
+      <translation>Valider la génération</translation>
+    </message>
+    <message>
+      <source>Send generation</source>
+      <translation>Lancer la génération</translation>
+    </message>
+    <message>
+      <source>Cancel generation</source>
+      <translation>Annuler la génération</translation>
+    </message>
+    <message>
+      <source>Do you really want to cancel the generation?</source>
+      <translation>Voulez-vous vraiment annuler la génération ?</translation>
+    </message>
+    <message>
+      <source>Choose the extensions to be crossed for the generation</source>
+      <translation>Choisissez les extensions à prendre en compte pour la génération des fichiers.</translation>
+    </message>
+    <message>
+      <source>Choose context</source>
+      <translation>Choisir un contexte</translation>
+    </message>
+    <message>
+      <source>Choose language</source>
+      <translation>Choisir une langue</translation>
+    </message>
+    <message>
+      <source>Search translation name</source>
+      <translation>Rechercher une clé de traduction</translation>
+    </message>
+    <message>
+      <source>Search</source>
+      <translation>Rechercher</translation>
+    </message>
+    <message>
+      <source>Reset</source>
+      <translation>Réinitialiser</translation>
+    </message>
+    <message>
+      <source>This extension aims at generating a file of translation by language, and at being able to modify them in the interface of the backOffice of eZ.</source>
+      <translation>Cette extension a pour but de générer un fichier de traduction par langue, et de fournir une interface permettant de modifier ces traductions dans le backOffice eZPublish.</translation>
+    </message>
+    <message>
+      <source>Before anything else, you have to generate the files of translations to be homogeneous.</source>
+      <translation>Avant toutes choses, il est nécessaire de générer les fichiers de traductions.</translation>
+    </message>
+    <message>
+      <source>Translator / List</source>
+      <translation>Traducteur / Liste</translation>
+    </message>
+    <message>
+      <source>translations</source>
+      <translation>traductions</translation>
+    </message>
+    <message>
+      <source>Translation name</source>
+      <translation>Clé de traduction</translation>
+    </message>
+    <message>
+      <source>Context name Translation</source>
+      <translation>Contexte de traduction</translation>
+    </message>
+    <message>
+      <source>Click to open</source>
+      <translation>Cliquer pour ouvrir</translation>
+    </message>
+    <message>
+      <source>Validate translation</source>
+      <translation>Valider la traduction</translation>
+    </message>
+    <message>
+      <source>Send traduction</source>
+      <translation>Envoyer la traduction</translation>
+    </message>
+    <message>
+      <source>Cancel translation</source>
+      <translation>Annuler la traduction</translation>
+    </message>
+    <message>
+      <source>Do you really want to cancel the translation?</source>
+      <translation>Voulez-vous vraiment annuler la traduction ?</translation>
+    </message>
+    <message>
+      <source>Edit translation</source>
+      <translation>Éditer la traduction</translation>
+    </message>
+  <message><source>List</source><translation>Liste</translation></message><message><source>Search translation key</source><translation>Rechercher une clé de traduction</translation></message><message><source>Translation key</source><translation>Clé de traduction</translation></message><message><source>Translator / Search</source><translation>Traducteur / Recherche</translation></message></context>
+</TS>
